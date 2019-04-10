a4ccde78cb6a8e5703de4c789111fa6440940b73
Daniel Maturana <dimatura@cmu.edu>
2019-04-10 05:32:54 -0400

    pid2 works now

--- scripts/pid2.py
+++ scripts/pid2.py
@@ -1,9 +1,10 @@
 
+import numpy as np
 import time
 import atexit
 import struct
 from cobs import cobs
-# from simple_pid import PID
+from simple_pid import PID
 
 import serial
 
@@ -40,8 +41,7 @@ def motor_map(val):
     # -1, 1 to 0, 1
     val2 = (val + 1.0)/2.
     val3 = MOTOR_MIN*(1.-val2) + val2*MOTOR_MAX
-    print val3
-    val4 = min(max(val3, MOTOR_MIN), MOTOR_MAX)
+    val4 = np.clip(val3, MOTOR_MIN, MOTOR_MAX)
     return int(val4)
 
 
@@ -55,27 +55,38 @@ print(ser.is_open)
 #maestro = exceed_bot.maestro.Controller('/dev/ttyACM0')
 maestro = exceed_bot.maestro.Controller('/dev/maestro')
 
-target_ticks = 2
+target_ticks = 8.0
 
-# pid = PID(Kp=0.01, Ki=0.0, Kd=0.0, setpoint=8.0, sample_time=0.01, output_limits=(MOTOR_MIN, MOTOR_MAX))
-#pid.auto_mode = True
-throttle_pulse0 = MOTOR_CENTER
-throttle_pulse1 = throttle_pulse0
-err0 = 0
+pid = PID(Kp=0.02, Ki=0.02, Kd=0.0, setpoint=target_ticks, sample_time=0.01, output_limits=(0.0, 1.0))
 
-Kp = 1.0
+
+print('calibration')
+maestro.setTarget(MOTOR, MOTOR_CENTER)
+time.sleep(1.0)
+
+
+tic = time.time()
 
 while True:
     ticks = read_msg()[0]
+    err = target_ticks - ticks
 
-    err = (target_ticks - ticks)
-    print('err: %d' % err)
-    correction = Kp*err 
+    throttle = pid(float(ticks))
+    throttle_pulse = motor_map(throttle)
 
-    throttle_pulse1 = throttle_pulse0 + correction
-    throttle_pulse1 = min(max(throttle_pulse1, MOTOR_MIN), MOTOR_MAX)
+    print(pid.setpoint, err, ticks, throttle, throttle_pulse/4.0)
 
-    print(ticks, throttle_pulse1/4.0)
     # note not multiplying by 4
-    maestro.setTarget(MOTOR, int(throttle_pulse1))
-    throttle_pulse0 = throttle_pulse1
+    maestro.setTarget(MOTOR, int(throttle_pulse))
+
+    elapsed = time.time() - tic
+    if elapsed > 2.0:
+        pid.setpoint = 0.0
+    if elapsed > 4.0:
+        pid.setpoint = 10.0
+    if elapsed > 6.0:
+        pid.setpoint = 4.0
+    if elapsed > 8.0:
+        pid.setpoint = 12.0
+    if elapsed > 10.0:
+        pid.setpoint = 8.0
