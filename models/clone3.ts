e3124b4dd15acc60c207bff6f7f589db47aecaaf
Daniel Maturana <dimatura@cmu.edu>
2019-04-12 05:22:46 -0400

    clone3

--- models/clone3.ts
+++ models/clone3.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:809b9f88bfc5798c7f1ab348a5be2dc78fb968c13fc1a8212e4dc5bda161b21b
-size 44805803
+oid sha256:c8cf47d8ace9325b07831086e63adffd6518a21313fa61721b8f9682e4b346b2
+size 44806169
