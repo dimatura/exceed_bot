095e36645801d9277058320e5bb5f59578a40f47
Daniel Maturana <dimatura@cmu.edu>
2019-04-12 08:33:08 -0400

    bla

---
+++ models/wp1_c2.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9b7e260e3905dd536f6bcf9853822978372aaea266639e370054111b089a2941
+size 44806169
---
+++ models/wp1_d4.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:68eca0d785bee765a5757f0ca8502b3003b40673fd4a3a599d1eb239151e6324
+size 44836955
