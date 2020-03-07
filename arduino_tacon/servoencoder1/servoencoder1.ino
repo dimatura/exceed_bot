d9c1a01cf26605b08659442e1bcf676fa24f793f
Daniel Maturana <dimatura@cmu.edu>
2020-03-07 09:39:43 -0500

    close

--- arduino_tacon/servoencoder1/servoencoder1.ino
+++ arduino_tacon/servoencoder1/servoencoder1.ino
@@ -9,6 +9,15 @@
 static constexpr int LED_PIN = 13;
 //static constexpr int SERVO2_PIN = 5;
 
+//static constexpr double DEFAULT_KP = 0.00005;
+//static constexpr double DEFAULT_KD = 0.0;
+//static constexpr double DEFAULT_KI = 0.000001;
+
+static constexpr double DEFAULT_KP = 0.0005;
+static constexpr double DEFAULT_KD = 0.0;
+static constexpr double DEFAULT_KI = 0.0;
+
+static constexpr long CMD_TIMEOUT_MS = 4000;
 
 struct GlobalContext {
   double ticks_per_s = 0.0;
@@ -16,6 +25,12 @@ struct GlobalContext {
   double ticks_error = 0.0;
   int steer_input_deg = 90;
   int motor_input_deg = 90;
+
+  double kp = DEFAULT_KP;
+  double ki = DEFAULT_KI;
+  double kd = DEFAULT_KD;
+  bool pid_gains_reset = false;
+
   elapsedMillis ms_since_last_input;
 } ctx;
 
@@ -27,15 +42,13 @@ struct InputMsg {
 
 
 struct CommsTask {
-  static constexpr size_t BUF_LEN = 256;
+  static constexpr size_t BUF_LEN = 1024;
   uint8_t input_buf[BUF_LEN];
   uint8_t output_buf[BUF_LEN];
   size_t inp_buf_ix = 0;
 
-  static const int input_msg_capacity = JSON_OBJECT_SIZE(2);
-  static const int output_msg_capacity = JSON_OBJECT_SIZE(4) + 60;
-  StaticJsonDocument<input_msg_capacity> input_doc;
-  StaticJsonDocument<output_msg_capacity> output_doc;
+  static const int input_msg_capacity = JSON_OBJECT_SIZE(2) + 80;
+  static const int output_msg_capacity = JSON_OBJECT_SIZE(6) + 120;
 
   bool error = false;
   bool overflow = false;
@@ -55,34 +68,55 @@ struct CommsTask {
 
   bool read_msg() {
     //int buf_read = Serial.readBytesUntil('\n', input_buf, BUF_LEN - 1);
+    StaticJsonDocument<input_msg_capacity> input_doc;
     bool received = false;
     while (Serial.available() > 0) {
       uint8_t data = Serial.read();
       if (data == '\n') {
-        // reset ms since last
-        ctx.ms_since_last_input = 0;
         // decode
-        DeserializationError err = deserializeJson(this->input_doc, this->input_buf, BUF_LEN-1);
+        DeserializationError err = deserializeJson(input_doc, this->input_buf, BUF_LEN-1);
         this->error = false;
         if (err == DeserializationError::Ok) {
-          JsonVariant tticks_var = this->input_doc["tticks"];
-          JsonVariant steer_input_deg_var = this->input_doc["tsteer"];
+          JsonVariant tticks_var = input_doc["target_ticks_per_s"];
           if (!tticks_var.isNull()) {
             ctx.target_ticks_per_s = tticks_var.as<double>();
           } else {
             this->error = true;
           }
 
+          JsonVariant steer_input_deg_var = input_doc["target_steer_deg"];
           if (!steer_input_deg_var.isNull()) {
             ctx.steer_input_deg = steer_input_deg_var.as<int>();
           } else {
             this->error = true;
           }
+
+          // these are optional
+          JsonVariant kp = input_doc["kp"];
+          if (!kp.isNull()) {
+            ctx.kp = kp.as<double>();
+            ctx.pid_gains_reset = true;
+          }
+          JsonVariant ki = input_doc["ki"];
+          if (!ki.isNull()) {
+            ctx.ki = ki.as<double>();
+            ctx.pid_gains_reset = true;
+          }
+          JsonVariant kd = input_doc["kd"];
+          if (!kd.isNull()) {
+            ctx.kd = ki.as<double>();
+            ctx.pid_gains_reset = true;
+          }
+
         } else {
           this->error = true;
         }
         this->inp_buf_ix = 0;
         this->overflow = false;
+        if (!error) {
+          // reset ms since last only if ok
+          ctx.ms_since_last_input = 0;
+        }
         received = true;
       } else {
         // add to buffer
@@ -99,11 +133,16 @@ struct CommsTask {
   }
 
   void write_msg() {
-    this->output_doc["tticks"] = ctx.target_ticks_per_s;
-    this->output_doc["tsteer"] = ctx.steer_input_deg;
-    this->output_doc["ticks"] = ctx.ticks_per_s;
-    this->output_doc["motor"] = ctx.motor_input_deg;
-    size_t written = serializeJson(this->output_doc, this->output_buf, BUF_LEN-2);
+    StaticJsonDocument<output_msg_capacity> output_doc;
+    output_doc["target_ticks_per_s"] = ctx.target_ticks_per_s;
+    output_doc["target_steer_deg"] = ctx.steer_input_deg;
+    output_doc["ticks_per_s"] = ctx.ticks_per_s;
+    output_doc["motor_input_deg"] = ctx.motor_input_deg;
+    output_doc["ms_since_last_input"] = static_cast<long>(ctx.ms_since_last_input);
+    if (error) {
+      output_doc["error"] = 1;
+    }
+    size_t written = serializeJson(output_doc, this->output_buf, BUF_LEN-2);
     output_buf[written] = '\n';
     output_buf[written+1] = '\0';
     Serial.write(output_buf, written+1);
@@ -174,14 +213,11 @@ struct MotorControlTask {
 
   static constexpr int SERVO_MOTOR_PIN = 4;
 
-  static constexpr double KP = 0.00005;
-  static constexpr double KD = 0.0;
-  static constexpr double KI = 0.000001;
-
   PWMServo servo;
   TimedPID motor_pid;
+  double out;
 
-  MotorControlTask() : motor_pid(KP, KI, KD) { }
+  MotorControlTask() : motor_pid(DEFAULT_KP, DEFAULT_KI, DEFAULT_KD), out(90.0) { }
 
   void setup() {
     pinMode(SERVO_MOTOR_PIN, OUTPUT);
@@ -195,14 +231,25 @@ struct MotorControlTask {
   }
 
   void run() {
-    double inp = constrain(ctx.motor_input_deg, SERVO_MIN, SERVO_MAX);
-#if 0
-    double target = 8;
-    double error = (target - delta);
-    out += motor_pid.getCmdAutoStep(target, delta);
-    out = constrain(out, 90.0, 110.0);
-#endif
-    this->servo.write(inp);
+    if (ctx.pid_gains_reset) {
+      this->motor_pid.setGains(ctx.kp, ctx.ki, ctx.kd);
+      this->motor_pid.reset();
+      ctx.pid_gains_reset = false;
+      return;
+    }
+
+    if (ctx.ms_since_last_input > CMD_TIMEOUT_MS) {
+      // stop
+      this->servo.write(90);
+      return;
+    }
+
+    //double inp = constrain(ctx.motor_input_deg, SERVO_MIN, SERVO_MAX);
+    double error = ctx.target_ticks_per_s - ctx.ticks_per_s;
+    ctx.ticks_error = error;
+    out += motor_pid.getCmdAutoStep(ctx.target_ticks_per_s, ctx.ticks_per_s);
+    out = constrain(out, SERVO_MIN, SERVO_MAX);
+    this->servo.write(static_cast<int>(out));
   }
 
 } motor_control_task;
@@ -220,6 +267,12 @@ struct SteerControlTask {
   }
 
   void run() {
+    if (ctx.ms_since_last_input > CMD_TIMEOUT_MS) {
+      // stop
+      this->servo.write(90);
+      return;
+    }
+
     double inp = constrain(ctx.steer_input_deg, SERVO_MIN, SERVO_MAX);
     this->servo.write(inp);
   }
