55ff56475fecc3cc61ab317cf75e0413d40ca2fb
Daniel Maturana <dimatura@cmu.edu>
2020-03-17 05:03:22 -0400

    new protocol: array w/ cmd in first part, params in second part

--- arduino_tacon/servoencoder4/comms_task.h
+++ arduino_tacon/servoencoder4/comms_task.h
@@ -27,8 +27,8 @@ struct CommsTask {
   uint8_t output_buf[BUF_LEN];
   size_t inp_buf_ix = 0;
 
-  // overkill sizes
-  static const int input_msg_capacity = JSON_OBJECT_SIZE(8) + 160;
+  // overkill sizes. this would be around 300 bytes
+  static const int input_msg_capacity = JSON_ARRAY_SIZE(2) + JSON_OBJECT_SIZE(8) + 160;
   static const int output_msg_capacity = JSON_OBJECT_SIZE(8) + 160;
 
   bool error = false;
@@ -47,66 +47,96 @@ struct CommsTask {
     //this->packets.setPacketHandler(&on_packet);
   }
 
-  void handle_motor_cmd(StaticJsonDocument<input_msg_capacity>& input_doc, GlobalContext* ctx) {
-      JsonVariant tticks_var = input_doc[TARGET_TICKS_PER_S_KEY];
-      JsonVariant steer_input_deg_var = input_doc[STEER_INPUT_DEG_KEY];
+  bool handle_motor_cmd(JsonObject& input_doc, GlobalContext* ctx) {
+    JsonVariant tticks_var = input_doc[TARGET_TICKS_PER_S_KEY];
+    JsonVariant steer_input_deg_var = input_doc[STEER_INPUT_DEG_KEY];
 
-      if (!tticks_var.isNull()) { ctx->target_ticks_per_s = tticks_var.as<float>(); } else { this->error = true; }
-      if (!steer_input_deg_var.isNull()) { ctx->steer_input_deg = steer_input_deg_var.as<int>(); } else { this->error = true; }
-      this->reply_motor_cmd(ctx);
+    // set at least one of these
+    if (tticks_var.isNull() && steer_input_deg_var.isNull()) {
+      return false;
+    }
+
+    if (!tticks_var.isNull()) {
+      ctx->target_ticks_per_s = tticks_var.as<float>();
+    }
+    if (!steer_input_deg_var.isNull()) {
+      ctx->steer_input_deg = steer_input_deg_var.as<int>();
+    }
+
+    return true;
   }
 
-  void handle_pid_cmd(StaticJsonDocument<input_msg_capacity>& input_doc, GlobalContext* ctx) {
-      JsonVariant kp = input_doc["kp"];
-      JsonVariant ki = input_doc["ki"];
-      JsonVariant kd = input_doc["kd"];
+  bool handle_pid_cmd(JsonObject& input_doc, GlobalContext* ctx) {
+    JsonVariant kp = input_doc["kp"];
+    JsonVariant ki = input_doc["ki"];
+    JsonVariant kd = input_doc["kd"];
+    //TODO: check set at least one
 
-      if (!kp.isNull()) { ctx->kp = kp.as<float>(); ctx->pid_gains_reset = true; }
-      if (!ki.isNull()) { ctx->ki = ki.as<float>(); ctx->pid_gains_reset = true; }
-      if (!kd.isNull()) { ctx->kd = kd.as<float>(); ctx->pid_gains_reset = true; }
-      this->reply_pid_cmd(ctx);
+    if (!kp.isNull()) { ctx->kp = kp.as<float>(); ctx->pid_gains_reset = true; }
+    if (!ki.isNull()) { ctx->ki = ki.as<float>(); ctx->pid_gains_reset = true; }
+    if (!kd.isNull()) { ctx->kd = kd.as<float>(); ctx->pid_gains_reset = true; }
+    this->reply_pid_cmd(ctx);
+    return true;
   }
 
-  void handle_ping_cmd(GlobalContext *ctx) {
+  bool handle_ping_cmd(GlobalContext *ctx) {
     this->reply_ping_cmd(ctx);
+    return true;
   }
+
+  bool parse_cmd(GlobalContext* ctx) {
+    StaticJsonDocument<input_msg_capacity> input_doc;
+    // decode
+    DeserializationError err = deserializeJson(input_doc, this->input_buf, BUF_LEN-1);
+    if (err != DeserializationError::Ok) {
+      return false;
+    }
+
+    JsonArray docArr = input_doc.as<JsonArray>();
+    if (docArr.isNull() || docArr.size() != 2) {
+      return false;
+    }
+
+    //JsonVariant cmd_var = input_doc[CMD_TYPE_KEY];
+    JsonVariant cmd_var = docArr[0];
+    JsonVariant params_var = docArr[1];
+    if (cmd_var.isNull() || params_var.isNull()) {
+      return false;
+    }
+    const char * cmd = cmd_var.as<const char*>();
+    JsonObject params = params_var.as<JsonObject>();
+    if (params.isNull()) {
+      return false;
+    }
+    if (strncmp(cmd, MOTION_CMD_ID, MAX_CMD_LENGTH) == 0) {
+      return this->handle_motor_cmd(params, ctx);
+    } else if (strncmp(cmd, PID_CMD_ID, MAX_CMD_LENGTH) == 0) {
+      return this->handle_pid_cmd(params, ctx);
+    } else if (strncmp(cmd, PING_CMD_ID, MAX_CMD_LENGTH) == 0) {
+      return this->handle_ping_cmd(ctx);
+    } else {
+      // assume one of x commands
+      return false;
+    }
+    // shouldn't reach
+    return true;
+  }
+
 
   void run(GlobalContext* ctx) {
     //int buf_read = Serial.readBytesUntil('\n', input_buf, BUF_LEN - 1);
-    StaticJsonDocument<input_msg_capacity> input_doc;
     while (Serial.available() > 0) {
       uint8_t data = Serial.read();
       if (data == '\n') {
-        // decode
-        DeserializationError err = deserializeJson(input_doc, this->input_buf, BUF_LEN-1);
-        this->error = false;
-        if (err == DeserializationError::Ok) {
-          JsonVariant cmd_var = input_doc[CMD_TYPE_KEY];
-          if (!cmd_var.isNull()) {
-            const char * cmd = cmd_var.as<const char*>();
-            if (strncmp(cmd, MOTION_CMD_ID, MAX_CMD_LENGTH) == 0) {
-              this->handle_motor_cmd(input_doc, ctx);
-            } else if (strncmp(cmd, PID_CMD_ID, MAX_CMD_LENGTH) == 0) {
-              this->handle_pid_cmd(input_doc, ctx);
-            } else if (strncmp(cmd, PING_CMD_ID, MAX_CMD_LENGTH) == 0) {
-              this->handle_ping_cmd(ctx);
-            } else {
-              // assume one of x commands
-              this->error = true;
-            }
-          }
-        } else {
-          this->error = true;
-        }
+        this->error = !this->parse_cmd(ctx);
         this->inp_buf_ix = 0;
         this->overflow = false;
-        if (!error) {
+        if (!this->error) {
           // reset ms since last only if ok
           ctx->ms_since_last_input = 0;
         } else {
           this->reply_error(ctx);
         }
-
       } else {
         // add to buffer
         if (inp_buf_ix < (BUF_LEN - 1)) {
@@ -130,9 +160,6 @@ struct CommsTask {
     output_doc[TICKS_PER_S_KEY] = ctx->ticks_per_s;
     output_doc[MOTOR_INPUT_US_KEY] = ctx->motor_input_us;
     output_doc[MS_SINCE_LAST_INPUT_KEY] = static_cast<long>(ctx->ms_since_last_input);
-    if (error) {
-      output_doc[ERROR_KEY] = 1;
-    }
     if (!Serial.availableForWrite()) {
       return;
     }
@@ -148,9 +175,6 @@ struct CommsTask {
     output_doc["ki"] = ctx->ki;
     output_doc["kd"] = ctx->kd;
     output_doc[MS_SINCE_LAST_INPUT_KEY] = static_cast<long>(ctx->ms_since_last_input);
-    if (this->error) {
-      output_doc[ERROR_KEY] = 1;
-    }
     if (!Serial.availableForWrite()) {
       return;
     }
@@ -162,7 +186,7 @@ struct CommsTask {
 
   void reply_error(GlobalContext* ctx) {
     StaticJsonDocument<output_msg_capacity> output_doc;
-    output_doc["error"] = 1;
+    output_doc[ERROR_KEY] = 1;
     output_doc[MS_SINCE_LAST_INPUT_KEY] = static_cast<long>(ctx->ms_since_last_input);
     if (!Serial.availableForWrite()) {
       return;
