3c4ca4d0d7d90224792e2cf0251f69a65742de27
Daniel Maturana <dimatura@cmu.edu>
2020-08-01 01:33:54 -0400

    jsontest pid working ok w/ joystick

--- arduino_tacon/servoencoder5/jsontest1.py
+++ arduino_tacon/servoencoder5/jsontest1.py
@@ -12,15 +12,6 @@ import json
 
 fields = ['target_L', 'target_R', 'ticks_L', 'ticks_R', 'error_L', 'error_R', 'out_L', 'out_R']
 
-def recv_msg(ser):
-    s = ser.read_until(b'\n')
-    #print(s)
-    n = len(s)
-    #logging.info('read %d bytes', n)
-    if n > 0:
-        return json.loads(s[:(n-1)])
-
-
 def make_exit_handler(ser):
     def exit_handler():
         logging.info('closing port')
@@ -28,25 +19,50 @@ def make_exit_handler(ser):
     return exit_handler
 
 
+class CommWrapper:
+    def __init__(self):
+        #ser = serial.Serial('/dev/ttyACM0', 9600)
+        self.ser = serial.Serial('/dev/ttyACM0', 115200)
+        #ser = serial.Serial('/dev/zumo', 115200)
+        #ser = serial.Serial('/dev/ttyUSB0', 115200)
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+        atexit.register(make_exit_handler(self.ser))
+        logging.info('open: %r' % self.ser.is_open)
+        logging.info(self.ser.writable())
+
+    def send(self, msg, get_reply=True):
+        msg = json.dumps(msg) + '\n'
+        wrote = self.ser.write(msg.encode())
+        if get_reply:
+            reply = self.receive()
+            return reply
+        else:
+            return wrote
+
+    def receive(self):
+        s = self.ser.read_until(b'\n')
+        n = len(s)
+        if n > 0:
+            return json.loads(s[:(n-1)])
+        else:
+            return None
+
+
 def main():
+    comms = CommWrapper()
+
     for device in inputs.devices.gamepads:
         print(device)
 
-    #ser = serial.Serial('/dev/ttyACM0', 9600)
-    ser = serial.Serial('/dev/ttyACM0', 115200)
-    #ser = serial.Serial('/dev/zumo', 115200)
-    #ser = serial.Serial('/dev/ttyUSB0', 115200)
-    ser.reset_input_buffer()
-    ser.reset_output_buffer()
-    exit_handler = make_exit_handler(ser)
-    atexit.register(exit_handler)
-    logging.info('open: %r' % ser.is_open)
-    logging.info(ser.writable())
-
     time.sleep(1.0)
-    cmd = {"target_ticks_per_s": 0, "target_steer_deg": 90, "kp": 6e-4, "kd": 0, "ki": 0.0000}
-    msg = json.dumps(cmd) + '\n'
-    wrote = ser.write(msg.encode())
+
+    cmd = ["ping", {}]
+    pong = comms.send(cmd)
+
+    cmd = ["pid", {"kp": 0.0001, "kd": 0.0, "ki": 0.0006, "kf": 0.001}]
+    pid_reply = comms.send(cmd)
+
     time.sleep(0.5)
 
     if False:
@@ -61,7 +77,6 @@ def main():
             formatted_msg = ', '.join(["{}:{:8.2f}".format(k, v) for (k, v) in recv.items()])
             logging.info(formatted_msg)
             time.sleep(0.1)
-
 
     steer = 90
     throttle = 0
@@ -86,24 +101,16 @@ def main():
                     print('steer: %f' % steer)
                 elif event.code == 'ABS_RZ':
                     norm_throttle = -2.0*(float(event.state) - 128)/256.0
-                    throttle = 200*norm_throttle
-                    if abs(throttle) < 0.1:
+                    if abs(norm_throttle) < 0.1:
                         throttle = 0
+                    else:
+                        throttle = 200*norm_throttle
                     print('throttle: %f' % throttle)
 
 
-        cmd = {"target_ticks_per_s": throttle, "target_steer_deg": steer}
-        msg = json.dumps(cmd) + '\n'
-        #logging.info('writing')
-        #logging.info(msg)
-        wrote = ser.write(msg.encode())
-        #logging.info('wrote %d bytes', wrote)
-
-        # get ack
-        #logging.info('reading')
-        recv = recv_msg(ser)
-        #logging.info(recv)
-        formatted_msg = ', '.join(["{}:{:8.2f}".format(k, v) for (k, v) in recv.items()])
+        cmd = ["motion", {"target_ticks_per_s": throttle, "steer_input_deg": steer}]
+        motion_reply = comms.send(cmd)
+        formatted_msg = ', '.join(["{}:{:8.2f}".format(k, v) for (k, v) in motion_reply.items()])
         logging.info(formatted_msg)
         #time.sleep(1.)
 
