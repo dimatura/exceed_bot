fa5f05ec153d6d0f302a401d2a66a5a5a83b9aa9
Daniel Maturana <dimatura@cmu.edu>
2020-12-30 03:23:07 -0500

    steer tuning

--- arduino/servoencoder5/steer_task.h
+++ arduino/servoencoder5/steer_task.h
@@ -7,10 +7,12 @@
 
 
 struct SteerControlTask {
-  static constexpr int SERVO_CENTER_DEG = 90;
+  // note; 95 is based on tacon, using 90 makes it go to the left
+  static constexpr int SERVO_CENTER_DEG = 95;
   static constexpr int SERVO_MIN = SERVO_CENTER_DEG-28;
   static constexpr int SERVO_MAX = SERVO_CENTER_DEG+28;
   static constexpr int SERVO_STEER_PIN = 3;
+  static constexpr int SERVO_DEADZONE_DEG = 2;
 
   // note that each servo pulse is 20ms
   static constexpr long STEER_PERIOD_MS = 40;
@@ -21,21 +23,32 @@ struct SteerControlTask {
   void setup() {
     pinMode(SERVO_STEER_PIN, OUTPUT);
     this->servo.attach(SERVO_STEER_PIN);
+    this->servo.write(SERVO_CENTER_DEG);
     this->elapsed_ms = 0;
   }
 
   void run(GlobalContext* ctx) {
     if (ctx->ms_since_last_input > CMD_TIMEOUT_MS) {
-      // stop
+      // turns out tacon servo just holds position when it gets no pulses
+      // so detach does nothing
+      // if (this->servo.attached()) { this->servo.detach(); }
+      // this method doesn't do anything either
+      // this->servo.writeMicroseconds(500);
       this->servo.write(SERVO_CENTER_DEG);
       return;
     }
 
     if (this->elapsed_ms < STEER_PERIOD_MS) {
-        return;
+      return;
+    }
+
+    if (!this->servo.attached()) {
+      this->servo.attach(SERVO_STEER_PIN);
     }
 
     float inp = constrain(ctx->steer_input_deg, SERVO_MIN, SERVO_MAX);
+    // TODO detach instead?
+    // if ((inp >= -SERVO_DEADZONE_DEG) && (inp <= SERVO_DEADZONE_DEG)) { return; }
     this->servo.write(inp);
     this->elapsed_ms = 0;
   }
--- scripts/rosbag_export.py
+++ scripts/rosbag_export.py
@@ -3,63 +3,160 @@ import json
 import rosbag
 import cv2
 import numpy as np
+from tqdm import tqdm
+import numpy as np
 
 from path import Path
 
 
-parser = argparse.ArgumentParser(description='Export rosbag to images')
-parser.add_argument('rosbag', type=str)
-args = parser.parse_args()
-
-out_dir = Path(args.rosbag).stripext()
-print(out_dir)
-out_dir.makedirs_p()
-color_dir = (out_dir/'color').makedirs_p()
-depth_dir = (out_dir/'depth').makedirs_p()
+COLOR_TOPIC = '/camera/color/image_raw'
+DEPTH_TOPIC = '/camera/aligned_depth_to_color/image_raw'
 
 
+def main_default(args):
+    out_dir = Path(args.rosbag).stripext()
+    print(out_dir)
+    out_dir.makedirs_p()
+    color_dir = (out_dir/'color').makedirs_p()
+    depth_dir = (out_dir/'depth').makedirs_p()
 
-bag = rosbag.Bag(args.rosbag)
-for topic, msg, ts in bag.read_messages(topics=['/camera/color/camera_info']):
-    print(msg)
+    bag = rosbag.Bag(args.rosbag)
+    for topic, msg, ts in bag.read_messages(topics=['/camera/color/camera_info']):
+        print(msg)
+        # for open3d format, it's transposed
+        K2 = [msg.K[0], 0, 0, 0, msg.K[4], 0, msg.K[2], msg.K[5], 1]
+        intrinsic = {'width': msg.width, 'height': msg.height, 'intrinsic_matrix': K2}
+        with open(out_dir/'camera_intrinsic.json', 'w') as f:
+            f.write(json.dumps(intrinsic, indent=2))
 
-    # for open3d format, it's transposed
-    K2 = [msg.K[0], 0, 0, 0, msg.K[4], 0, msg.K[2], msg.K[5], 1]
-    intrinsic = {'width': msg.width, 'height': msg.height, 'intrinsic_matrix': K2}
-    with open(out_dir/'camera_intrinsic.json', 'w') as f:
-        f.write(json.dumps(intrinsic, indent=2))
-
-    with open(out_dir/'ldvo_intrinsic.txt', 'w') as f:
-        f.write('%d %d\n' % (msg.width, msg.height))
-        f.write('%f %f %f\n' % (msg.K[0], msg.K[1], msg.K[2]))
-        f.write('%f %f %f\n' % (msg.K[3], msg.K[4], msg.K[5]))
-        f.write('%f %f %f\n' % (msg.K[6], msg.K[7], msg.K[8]))
-        f.write('0.0 0.0 0.0 0.0 0.0\n')
-
-    break
+        with open(out_dir/'ldvo_intrinsic.txt', 'w') as f:
+            f.write('%d %d\n' % (msg.width, msg.height))
+            f.write('%f %f %f\n' % (msg.K[0], msg.K[1], msg.K[2]))
+            f.write('%f %f %f\n' % (msg.K[3], msg.K[4], msg.K[5]))
+            f.write('%f %f %f\n' % (msg.K[6], msg.K[7], msg.K[8]))
+            f.write('0.0 0.0 0.0 0.0 0.0\n')
+        break
 
 
-with open(out_dir/'rgb.txt', 'w') as f:
+    with open(out_dir/'rgb.txt', 'w') as f:
+        ctr = 0
+        for topic, msg, ts in bag.read_messages(topics=['/camera/color/image_raw']):
+            img = np.frombuffer(msg.data).view('u1').reshape((msg.height, msg.width, 3))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img_basename = '%06d.jpg' % ctr
+            cv2.imwrite(str(color_dir/img_basename), img)
+            f.write('%d.%d %s\n' % (msg.header.stamp.secs, msg.header.stamp.nsecs, ('color/%s' % img_basename)))
+            ctr += 1
+            # break
+
+
+    with open(out_dir/'depth.txt', 'w') as f:
+        ctr = 0
+        for topic, msg, ts in bag.read_messages(topics=['/camera/aligned_depth_to_color/image_raw']):
+            depth = np.frombuffer(msg.data).view('u2').reshape((msg.height, msg.width))
+            depth_basename = '%06d.png' % ctr
+            cv2.imwrite(str(depth_dir/depth_basename), depth)
+            f.write('%d.%d %s\n' % (msg.header.stamp.secs, msg.header.stamp.nsecs, ('depth/%s' % depth_basename)))
+            ctr += 1
+            # break
+
+    bag.close()
+
+
+def main_reorder(args):
+    out_dir = Path(args.rosbag).stripext()
+    print(out_dir)
+    out_dir.makedirs_p()
+    color_dir = (out_dir/'color').makedirs_p()
+    depth_dir = (out_dir/'depth').makedirs_p()
+
+    bag = rosbag.Bag(args.rosbag)
+    for topic, msg, ts in bag.read_messages(topics=['/camera/color/camera_info']):
+        print(msg)
+        # for open3d format, it's transposed
+        K2 = [msg.K[0], 0, 0, 0, msg.K[4], 0, msg.K[2], msg.K[5], 1]
+        intrinsic = {'width': msg.width, 'height': msg.height, 'intrinsic_matrix': K2}
+        with open(out_dir/'camera_intrinsic.json', 'w') as f:
+            f.write(json.dumps(intrinsic, indent=2))
+
+        with open(out_dir/'ldvo_intrinsic.txt', 'w') as f:
+            f.write('%d %d\n' % (msg.width, msg.height))
+            f.write('%f %f %f\n' % (msg.K[0], msg.K[1], msg.K[2]))
+            f.write('%f %f %f\n' % (msg.K[3], msg.K[4], msg.K[5]))
+            f.write('%f %f %f\n' % (msg.K[6], msg.K[7], msg.K[8]))
+            f.write('0.0 0.0 0.0 0.0 0.0\n')
+        break
+
+    color_msgs = []
+    depth_msgs = []
+
+    color_cnt = bag.get_message_count(COLOR_TOPIC)
+    depth_cnt = bag.get_message_count(DEPTH_TOPIC)
+
+    pbar = tqdm(total=(color_cnt + depth_cnt))
+    for topic, msg, ts in bag.read_messages(topics=[COLOR_TOPIC, DEPTH_TOPIC]):
+        if topic == COLOR_TOPIC:
+            color_msgs.append(msg)
+        elif topic == DEPTH_TOPIC:
+            depth_msgs.append(msg)
+        else:
+            raise ValueError('huh?')
+        pbar.update()
+    pbar.close()
+    print('sorting')
+    color_msgs.sort(key=lambda k: k.header.stamp.to_sec())
+    depth_msgs.sort(key=lambda k: k.header.stamp.to_sec())
+
+    color_ts = np.asarray([m.header.stamp.to_sec() for m in color_msgs])
+    depth_ts = np.asarray([m.header.stamp.to_sec() for m in depth_msgs])
+
+    print('matching')
+    pbar = tqdm(total=color_cnt)
+    color_depth_pairs = []
+    for i, cts in enumerate(color_ts):
+        diff = np.abs(cts - depth_ts)
+        nz = np.sum(diff == 0)
+        # print('%d, eq: %d' % (i, nz))
+        if nz == 0:
+            continue
+        nzix = np.nonzero(diff == 0)[0]
+        color_depth_pairs.append((i, nzix[0]))
+        pbar.update()
+    pbar.close()
+
+    print('writing')
     ctr = 0
-    for topic, msg, ts in bag.read_messages(topics=['/camera/color/image_raw']):
-        img = np.frombuffer(msg.data).view('u1').reshape((msg.height, msg.width, 3))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img_basename = '%06d.jpg' % ctr
-        cv2.imwrite(str(color_dir/img_basename), img)
-        f.write('%d.%d %s\n' % (msg.header.stamp.secs, msg.header.stamp.nsecs, ('color/%s' % img_basename)))
-        ctr += 1
-        # break
+    pbar = tqdm(total=len(color_depth_pairs))
+    with open(out_dir/'rgb.txt', 'w') as cf, open(out_dir/'depth.txt', 'w') as df:
+        for cix, dix in color_depth_pairs:
+            cmsg = color_msgs[cix]
+            dmsg = depth_msgs[dix]
+
+            img = np.frombuffer(cmsg.data).view('u1').reshape((cmsg.height, cmsg.width, 3))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img_basename = '%06d.jpg' % ctr
+            cv2.imwrite(str(color_dir/img_basename), img)
+            cf.write('%d.%d %s\n' % (cmsg.header.stamp.secs, cmsg.header.stamp.nsecs, ('color/%s' % img_basename)))
+
+            depth = np.frombuffer(dmsg.data).view('u2').reshape((dmsg.height, dmsg.width))
+            depth_basename = '%06d.png' % ctr
+            cv2.imwrite(str(depth_dir/depth_basename), depth)
+            df.write('%d.%d %s\n' % (dmsg.header.stamp.secs, dmsg.header.stamp.nsecs, ('depth/%s' % depth_basename)))
+
+            ctr += 1
+            pbar.update()
+    pbar.close()
 
 
-with open(out_dir/'depth.txt', 'w') as f:
-    ctr = 0
-    for topic, msg, ts in bag.read_messages(topics=['/camera/aligned_depth_to_color/image_raw']):
-        depth = np.frombuffer(msg.data).view('u2').reshape((msg.height, msg.width))
-        depth_basename = '%06d.png' % ctr
-        cv2.imwrite(str(depth_dir/depth_basename), depth)
-        f.write('%d.%d %s\n' % (msg.header.stamp.secs, msg.header.stamp.nsecs, ('depth/%s' % depth_basename)))
-        ctr += 1
-        # break
 
-
-bag.close()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Export rosbag to images')
+    parser.add_argument('rosbag', type=str)
+    parser.add_argument('--reorder', action='store_true')
+    args = parser.parse_args()
+    print(args)
+    if args.reorder:
+        main_reorder(args)
+    else:
+        # main_default(args)
+        pass
